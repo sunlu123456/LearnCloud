d3d26206f45cd944050dbc2d6f3ae27139519af1
sunlu123456 <845534336@qq.com>
2021-09-14 17:44:35 +0800

    haha

---
+++ cloud-config-client-3355/src/main/java/com/lun/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.lun.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //未配置消息总线情况下，需发送Post请求：curl -X POST "http://localhost:3355/actuator/refresh"
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+}
