3098766a7d82c4d1a52a4b13c47395c42e47927b
sunlu123456 <845534336@qq.com>
2021-09-15 10:54:54 +0800

    hahha

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/lun/springcloud/controller/SendMessageController.java
@@ -0,0 +1,21 @@
+package com.lun.springcloud.controller;
+
+import com.lun.springcloud.service.IMessageProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage() {
+        return messageProvider.send();
+    }
+}
